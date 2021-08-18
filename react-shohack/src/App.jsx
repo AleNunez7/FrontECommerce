768c9ead44be328968d3861e3616d0541045e2aa
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-08-18 11:55:47 -0300

    Se agrego la ruta a producto

--- react-shohack/src/App.jsx
+++ react-shohack/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Products from "./Components/Products/Products";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
+import Product from "./Components/Product/Product";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/producto" component={Product} />
           <Route path="/productos" component={Products} />
           <Route path="/carrito" component={ShoppingCart} />
         </Switch>
