b8109508060e9a0ed1ce6a59fd560dc20c1fec22
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-08-20 15:46:29 -0300

    Se cambio texto en el banner

--- react-shohack/src/Components/Home/Banner/Banner.jsx
+++ react-shohack/src/Components/Home/Banner/Banner.jsx
@@ -6,9 +6,9 @@ function Banner() {
       <section className=" py-5 text-center container">
         <div className="row py-lg-5">
           <div className="divColor col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light mt-4"> NO TE QUEDES SIN </h1>
+            <h1 className="fw-light mt-4 fs-1">SHOHACK</h1>
             <h2 className="lead mt-4">
-              Café en la puerta de tu casa cada dos semanas a solo $900
+              Los mejores championes importados del exterior
             </h2>
             <Link to="/registro" className="btn btn-light rounded mt-4">
               REGISTRARSE
