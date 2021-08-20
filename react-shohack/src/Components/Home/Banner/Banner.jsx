f38174900e71ee069f538e9f1cfe88d971ae850b
SantiagoQuintela <s.quintelad@gmail.com>
2021-08-20 12:27:52 -0300

    se cambio la imagen del banner

--- react-shohack/src/Components/Home/Banner/Banner.jsx
+++ react-shohack/src/Components/Home/Banner/Banner.jsx
@@ -6,8 +6,10 @@ function Banner() {
       <section className=" py-5 text-center container">
         <div className="row py-lg-5">
           <div className="divColor col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light mt-4"> NO TE QUEDES SIN TOMAR CAFÉ</h1>
-            <h2 className="lead mt-4">Café en la puerta de tu casa cada dos semanas a solo $900</h2>
+            <h1 className="fw-light mt-4"> NO TE QUEDES SIN </h1>
+            <h2 className="lead mt-4">
+              Café en la puerta de tu casa cada dos semanas a solo $900
+            </h2>
             <button type="button" class="btn btn-light rounded mt-4">
               Unite al culto
             </button>
