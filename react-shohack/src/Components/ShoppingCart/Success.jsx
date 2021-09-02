3b35ff45cf9964e71a9fea65583a0232f011acfb
AleNunez7 <nunezalejandro019@gmail.com>
2021-09-02 12:11:43 -0300

    se hizo cambio visual en success

--- react-shohack/src/Components/ShoppingCart/Success.jsx
+++ react-shohack/src/Components/ShoppingCart/Success.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Success() {
   return (
-    <div className="text-center">
-      <h1>GRACIAS</h1>
-
-      <h2>POR SU COMPRA</h2>
-      <button>Volver a productos</button>
-      <button>Ver orden</button>
+    <div className="text-center my-4">
+      <div className="my-4">
+        <h1>GRACIAS</h1>
+        <h2>POR SU COMPRA</h2>
+      </div>
+      <Link to="/productos">
+        <button className="btn btn-dark mx-2 text-center">Volver a productos</button>
+      </Link>
     </div>
   );
 }
