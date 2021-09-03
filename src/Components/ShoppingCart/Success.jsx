702f0da78a6186ed53b60de77c35aab50d6e0374
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-09-03 13:05:30 -0300

    Se corrigue success

--- src/Components/ShoppingCart/Success.jsx
+++ src/Components/ShoppingCart/Success.jsx
@@ -4,11 +4,13 @@ function Success() {
   return (
     <div className="text-center my-5">
       <div className="my-4">
-        <h1>SU COMPRA FUE REALIZADA</h1>
-        <h2>GRACIAS!</h2>
+        <h1 className="mb-2">SU COMPRA FUE REALIZADA CON EXITO !</h1>
+        <h2 className="mb-2">GRACIAS!</h2>
       </div>
       <Link to="/productos">
-        <button className="btn btn-dark mx-2 text-center">Volver a productos</button>
+        <button className="btn btn-dark mx-2 text-center">
+          Volver a productos
+        </button>
       </Link>
     </div>
   );
