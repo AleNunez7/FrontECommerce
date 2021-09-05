bc4b6741f8fe448484b3265fad5f77af0c59eea5
AleNunez7 <nunezalejandro019@gmail.com>
2021-09-04 22:29:15 -0300

    se corrigió texto en sobre nosotros

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -74,7 +74,8 @@ function AboutUs() {
         <div className="text-start fs-4 mb-5">
           <p>
             Link para ingresar a la vista de Administrador: <br /> https://admin-shohack.vercel.app
-            y roles. <br /> También se podrán ver las ordenes realizadas por cada usuario. <br />
+            En esta sección se podrá leer, crear, modificar y eliminar usuarios, productos y roles.
+            <br /> También se podrán ver las ordenes realizadas por cada usuario. <br />
             Usuario: admin <br /> Password: admin
           </p>
         </div>
